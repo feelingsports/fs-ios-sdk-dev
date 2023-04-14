7a8a2b60efd8dab0fcac592471f9458c4c160519
Matthieu <matthieu@Matthieus-MacBook-Pro.local>
2023-04-14 15:26:50 +0200

    1.0.0

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "fsiossdk",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "fsiossdk",
+            targets: ["fsiossdk_wrapper"])
+    ],
+    dependencies: [
+        .package(name: "SocketIO", url: "https://github.com/socketio/socket.io-client-swift", .upToNextMinor(from: "16.0.0")),
+        .package(name: "Facebook", url: "https://github.com/facebook/facebook-ios-sdk", .upToNextMajor(from: "15.0.0"))
+    ],
+    targets: [
+        .target(
+            name: "fsiossdk_wrapper",
+            dependencies: [
+                .product(name: "FacebookLogin", package: "Facebook"),
+                .product(name: "SocketIO", package: "SocketIO"),
+                .target(name: "fsiossdk")
+            ],
+            path: "Sources/Wrapper",
+            publicHeadersPath: ""
+        ),
+        .binaryTarget(
+            name: "fsiossdk",
+            path: "fsiossdk.xcframework")
+    ])
