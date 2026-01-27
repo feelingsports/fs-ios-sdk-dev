8ff41b86026d1d9f7247b9b0757276562333c4b7
fstibolt <105725848+fstibolt@users.noreply.github.com>
2026-01-27 13:10:14 +0100

    2.4.2

--- Package.swift
+++ Package.swift
@@ -1,26 +1,13 @@
-// swift-tools-version:5.3
-import PackageDescription
-
-let package = Package(
-    name: "fsiossdk",
-    platforms: [
-        .iOS(.v12)
-    ],
-    products: [
-        .library(
-            name: "fsiossdk",
-            targets: ["fsiossdk_wrapper"])
-    ]
-    targets: [
-        .target(
-            name: "fsiossdk_wrapper",
-            dependencies: [
-                .target(name: "fsiossdk")
-            ],
-            path: "Sources/Wrapper",
-            publicHeadersPath: ""
-        ),
-        .binaryTarget(
-            name: "fsiossdk",
-            path: "fsiossdk.xcframework")
-    ])
+targets: [
+    .target(
+        name: "fsiossdk_wrapper",
+        dependencies: [
+            .target(name: "fsiossdk")
+        ],
+        path: "Sources/Wrapper"
+    ),
+    .binaryTarget(
+        name: "fsiossdk",
+        path: "fsiossdk.xcframework"
+    )
+]
