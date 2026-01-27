0d7a60c32a74235995c8b466943eb7e2c09edfba
fstibolt <105725848+fstibolt@users.noreply.github.com>
2026-01-27 13:12:53 +0100

    2.4.3

--- Package.swift
+++ Package.swift
@@ -1,13 +1,28 @@
-targets: [
-    .target(
-        name: "fsiossdk_wrapper",
-        dependencies: [
-            .target(name: "fsiossdk")
-        ],
-        path: "Sources/Wrapper"
-    ),
-    .binaryTarget(
-        name: "fsiossdk",
-        path: "fsiossdk.xcframework"
-    )
-]
+// swift-tools-version:6.2
+import PackageDescription
+
+let package = Package(
+    name: "fs-ios-sdk-dev",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "fs-ios-sdk-dev",
+            targets: ["fsiossdk_wrapper"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "fsiossdk_wrapper",
+            dependencies: [
+                .target(name: "fsiossdk")
+            ],
+            path: "Sources/Wrapper"
+        ),
+        .binaryTarget(
+            name: "fsiossdk",
+            path: "fsiossdk.xcframework"
+        )
+    ]
+)
